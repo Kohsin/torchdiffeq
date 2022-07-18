e369cd29238c73b84e1df296f37785cbfe800986
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-07-18 15:55:49 +0800

    Update odenet_mnist.py

--- examples/odenet_mnist.py
+++ examples/odenet_mnist.py
@@ -455,7 +455,7 @@ if __name__ == '__main__':
                     if name == '7.odefunc.conv1._layer.weight':
                         svs = svd(param.detach().cpu().numpy(), compute_uv=False)
                         sv.append(svs)
-                        test = svs.cpu().numpy()
+                        test = svs.numpy()
                         print('7.odefunc.conv1._layer.weight:',param.size())
                         print('sv:', test.shape())
                 train_acc = accuracy(model, train_eval_loader)
