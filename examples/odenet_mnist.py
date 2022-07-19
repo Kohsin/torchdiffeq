7e3b44e10e41390a2b300ace60ad3349b7a38a09
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-07-19 16:42:15 +0800

    Update odenet_mnist.py

--- examples/odenet_mnist.py
+++ examples/odenet_mnist.py
@@ -46,6 +46,7 @@ else:
 
 
 def jacobian(inputs, outputs):
+    inputs = Variable(inputs).to(device).requires_grad_()
     return torch.stack([grad([outputs[:, i].sum()], [inputs], retain_graph=True, create_graph=True)[0] for i in range(outputs.size(1))], dim=-1)
 
 
