62406d8c305e803d9fdddeb76846bc4492caf813
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-07-20 10:39:17 +0800

    Update odenet_mnist.py

--- examples/odenet_mnist.py
+++ examples/odenet_mnist.py
@@ -479,10 +479,9 @@ if __name__ == '__main__':
 
         #Jy = logits
         if len(Jx) > 0 and itr % batches_per_epoch == 0:
-            print(Jx)
-            #J = jacobian_temp(Jx[-1], Jy[-1])
-            #sv.append(svd(J, compute_uv=False))
-            #print('sv.len: ', len(sv))
+            J = jacobian(Jx[-1], Jy[-1])
+            sv.append(svd(J, compute_uv=False))
+            print('sv.len: ', len(sv))
 
         if is_odenet:
             nfe_forward = feature_layers[0].nfe
