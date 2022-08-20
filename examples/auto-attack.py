d1c9106abddd1fed13f8e92da15dca4da5b6563c
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-08-20 10:53:12 +0800

    Update auto-attack.py

--- examples/auto-attack.py
+++ examples/auto-attack.py
@@ -214,7 +214,8 @@ if __name__ == '__main__':
     feature_layers = [ODEBlock(ODEfunc(64))]
     fc_layers = [norm(64), nn.ReLU(inplace=True), nn.AdaptiveAvgPool2d((1, 1)), Flatten(), nn.Linear(64, 10)]
     #model = nn.Sequential(*downsampling_layers, *feature_layers, *fc_layers)
-    model = torch.load(model,'./experiment1.pkl')
+    model= torch.load('./experiment1.pkl')
+    
     model.cuda()
     model.eval()
 
